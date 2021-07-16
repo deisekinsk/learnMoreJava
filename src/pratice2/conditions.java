4e84eefcdefebc6ec69f0564452585056360262e
deisesilva <kinsk@msn.com>
2021-07-15 21:01:02 -0300

    if - else java

--- src/pratice2/conditions.java
+++ src/pratice2/conditions.java
@@ -5,10 +5,34 @@
  */
 package pratice2;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Deise Kinsk
  */
 public class conditions {
+    
+    public static void main (String [] args){
+        int studentGrade = 0;
+        
+        Scanner inputA = new Scanner(System.in);
+        System.out.println("Enter your data number: ");
+        
+        studentGrade = inputA.nextInt();
+        
+        if (studentGrade >= 60){
+            
+            System.out.println("Passed");
+        
+        } else if (studentGrade <= 59 && studentGrade >= 40) {
+        
+         System.out.println("Try again.");
+       
+        } else{
+            System.out.println("You don't conclude in this time.");
+        }
+    
+    }
     
 }
