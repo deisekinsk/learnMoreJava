5c396ccf790948c14ffc6d6e2197efdf6ccb6891
deisesilva <kinsk@msn.com>
2021-07-16 11:10:23 -0300

    swap two float numbers with three variables

---
+++ src/pratice2/swapNumbers.java
@@ -0,0 +1,40 @@
+
+package pratice2;
+
+/**
+ *
+ * @author Deise Kinsk
+ */
+
+import java.util.Scanner;
+
+public class swapNumbers {
+    //class
+    public static void main (String [] args){
+    //variables
+    float numA, numB,numEmpty;
+    
+    //Input
+    Scanner input = new Scanner(System.in);
+    
+    System.out.println("Enter your 1º number | with decimal (ex: 2.0): ");
+     numA = input.nextFloat();
+    //Scanner inputB = new Scanner(System.in);
+    System.out.print("Enter your 2º number | with decimal: ");
+    numB = input.nextFloat();
+    
+    //logical
+    numEmpty = numA;
+    numA = numB;
+    numB = numEmpty;
+    
+    float mathA = numB/numA;
+    float mathB = numA/numB;
+    
+    //print
+    System.out.println("First divide in your sequence: " + numB + " % "+numA+" = "+mathA);
+    System.out.println("When you inverte the numbers: " + numA + " % "+numB+" = "+mathB);
+    } 
+    
+    
+}
