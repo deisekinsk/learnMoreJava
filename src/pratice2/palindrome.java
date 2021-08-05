44f626ddd0554a2faa256b8c41a958bb27fc5d09
kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-08-04 22:01:52 -0300

    Create palindrome.java

---
+++ src/pratice2/palindrome.java
@@ -0,0 +1,39 @@
+//ERRO OUTPUT!
+
+import java.util.*; 
+import java.io.*;
+
+class Main {
+
+  public static String Palindrome(String str) {
+    // code goes here 
+
+    int len = str.length();
+
+    String invertW = " ";
+
+    for(int i = len-1; i >= 0; i-- ){
+
+      invertW = invertW + str.charAt(i);
+      System.out.println(invertW);
+
+    }
+
+    
+
+    if(str.equals(invertW)){
+      return "false";
+    }else{
+      return "true";
+    
+    }
+
+  }
+
+  public static void main (String[] args) {  
+    // keep this function call here     
+    Scanner s = new Scanner(System.in);
+    System.out.print(Palindrome(s.nextLine())); 
+  }
+
+}
