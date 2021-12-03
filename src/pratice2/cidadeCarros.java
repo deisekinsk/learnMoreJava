c6a86637595cf53ce46b8793bb24809fe780f4ea
Deise Kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-12-03 11:06:30 -0300

    Create cidadeCarros.java

---
+++ src/pratice2/cidadeCarros.java
@@ -0,0 +1,29 @@
+Estatística das Cidades (Paraná)
+
+        cod    numVei          numAcid
+Curitiba        033    1000          100
+Londrina        055    1300          247
+Maringá        087    3400          030
+
+
+int codCidade;
+int numVeiculosPasseio;
+int numAcidentesComVitimas;
+int mediaVeiculos;
+
+
+if(numAcid[i] > numAcid[i+1]){
+    System.out.print("O maior índice de acidentes é" + numAcid);
+}else{
+    System.out.print("O menor índice de acidentes é" + numAcid);
+}
+
+
+
+System.out.println("A média de veículos é" + media);
+
+
+
+if(numVei < 2000){
+    System.out.println("A média de veículos é" + media);
+}
