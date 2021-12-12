2caf1e7818f768fbd0ca7333ca563440dff686ca
Deise Kinsk <64823343+deisekinsk@users.noreply.github.com>
2021-12-11 21:23:16 -0300

    Update Values.java

--- notasGraduate/Values.java
+++ notasGraduate/Values.java
@@ -34,7 +34,7 @@ public class Values {
         Values disciplinaB = new Values(6.0, 9.0,9.0,9.0, 9.0, "Fundamentos P/ Computador");
         Values disciplinaC = new Values(8.0, 7.0,6.0,10.0, 9.0, "Teorias do Sistemas da Info.");
         Values disciplinaD = new Values(10.0, 9.0,1.0,10.0,8.0, "Matemática");
-        Values disciplinaE = new Values(10.0, 0.0, 6.0, 0.0, 10.0, "Lógica | Atrasada");
+        Values disciplinaE = new Values(10.0, 0.0, 6.0, 0.0, 10.0, "Lógica");
         
         System.out.println("Universidade Anhembi Morumbi\nGraduação Análise e Desenvolvimento de Sistemas \nMÉDIA FINAL POR DISCIPLINA(AP >=6) \n");
         System.out.println("- 1º Semestre: 02/2021 -");
@@ -43,6 +43,7 @@ public class Values {
         disciplinaB.printData();
         disciplinaC.printData();
         disciplinaD.printData();
+        disciplinaE.printData();
     }
 
 }
